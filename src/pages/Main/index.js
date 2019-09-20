52e60757217a47d2277ba2ea443bf686e835931a
Leozera <leobhal13@gmail.com>
2019-09-20 11:17:10 -0300

    Adicionando Icones

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Header from '~/components/Header';
+
 import { Container} from './styles.js';
 
 export default function Main() {
   return (
-    <Container/>
+    <Container>
+
+    </Container>
   );
 }
