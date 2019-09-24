faba7ba685c1633cff3d11d3983b43029b2f03c0
Leozera <leobhal13@gmail.com>
2019-09-24 13:43:42 -0300

    Estilizando nosso conteudo Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 
 import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
 
-import { Container } from './styles.js';
+
+import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles.js';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 2.000.000,65</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 200,00 recebida de Bruno Radamés hoje ás 06:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+      <Tabs />
     </Container>
   );
 }
